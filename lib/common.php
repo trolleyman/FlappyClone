f77dcbbbbddc149782f591de57bccd91e77a652d
Callum Tolley <cgtrolley@gmail.com>
2016-12-28 17:00:19 +0000

    Fix leaderboard path

--- lib/common.php
+++ lib/common.php
@@ -3,7 +3,7 @@
 	//header("Access-Control-Allow-Origin: *");
 	
 	// NB: If moving this file, correct this path.
-	const LEADERBOARD_PATH = __DIR__ . '\\..\\..\\PrivateData\\leaderboard.json';
+	const LEADERBOARD_PATH = 'C:\\Dev\\FlappyClone\\PrivateData\\leaderboard.json';
 	
 	include('lock.php');
 	include('name.php');
