68c7692df193c38c336b2bacf3e9024a2e5736a5
Callum Tolley <cgtrolley@gmail.com>
2017-01-02 21:22:57 +0000

    Updated update.sh

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ git pull
 rm -rf ./js 2> /dev/null # Don't care about errors
 mkdir js
 ./uglify.sh
-forever stop ws/server.js
+forever stop ws/server.js || echo "Warning: Could not stop ws/server.js"
 forever start ws/server.js
