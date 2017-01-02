636d59aa9edf4138e72ca18affbc8a761ae82251
Callum Tolley <cgtrolley@gmail.com>
2017-01-02 21:21:06 +0000

    Updated update.sh script

--- uglify.sh
+++ uglify.sh
@@ -1,2 +1,1 @@
 uglifyjs --mangle --wrap -o js/game.js -- src/fontfaceonload.js src/intersection.js src/cookie.js src/score.js src/button.js src/rect.js src/draw.js src/pipe.js src/bird.js src/net.js src/game.js
-return $?
