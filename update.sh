aabde5c5292702ecdfe5408a882760d8df2b2716
Callum Tolley <cgtrolley@gmail.com>
2017-01-02 21:18:07 +0000

    Update update.sh script

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
-git pull
+echo "Pulling updates from Git..."
+git pull || { echo "Git pull failed."; exit 1 }
 rm -rf ./js 2> /dev/null # Don't care about errors
 mkdir js
-./uglify.sh
+./uglify.sh || { echo "Uglify.js failed."; exit 1 }
+forever stop ws/server.js || { echo "Could not stop WebSockets server."; exit 1 }
+forever start ws/server.js || { echo "Could not start WebSockets server."; exit 1 }
