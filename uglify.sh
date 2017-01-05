76806b8bf85a0ad60ee53f50ce279e392a466f67
Callum Tolley <cgtrolley@gmail.com>
2017-01-05 02:21:47 +0000

    Updated update.sh

--- uglify.sh
+++ uglify.sh
@@ -1,1 +1,3 @@
+set -e
+set -x
 uglifyjs --mangle --wrap -o js/game.js -- src/fontfaceonload.js src/intersection.js src/cookie.js src/score.js src/button.js src/rect.js src/draw.js src/pipe.js src/bird.js src/game.js
--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
+set -e
+set -x
 git pull
 rm -rf ./js 2> /dev/null # Don't care about errors
 mkdir js
