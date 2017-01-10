7e7b5e0ac76df10e5d2ea8d510f51afe29425b6c
Callum Tolley <cgtrolley@gmail.com>
2017-01-10 18:33:52 +0000

    Try #2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		
 		<!-- Resizes viewport to look good on mobile -->
 		<!-- http://stackoverflow.com/questions/11689353/disable-pinch-zoom-on-mobile-web -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+		<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
 		<meta name="HandheldFriendly" content="true"/>
 		
 		<!-- Icon stuff. Generated using http://realfavicongenerator.net -->
