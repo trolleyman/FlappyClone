65c59ece9ac683d25f74fe96a78553280c30f1f3
Callum Tolley <cgtrolley@gmail.com>
2017-04-10 11:22:32 +0100

    Added clean.bat

--- clean.bat
+++ clean.bat
@@ -7,8 +7,5 @@ pushd "%~dp0/static/FlappyClone"
 rmdir /S /Q js
 mkdir js
 
-:: Uglify JS files
-CALL uglifyjs --mangle --wrap -o js/game.js -- src/*.js src/game/*.js src/common/*.js
-
 :: Reset dir
 popd
