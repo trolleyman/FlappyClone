45f73a5e8995a8280b22f4cbd090ffceac94d1e9
Callum Tolley <cgtrolley@gmail.com>
2018-08-19 00:44:02 +0100

    Fixed flake8 errors

--- admin.py
+++ admin.py
@@ -4,16 +4,23 @@ from django.contrib.auth.models import User
 
 from .models import UserProfile
 
-# Define an inline admin descriptor for UserProfile model
-# which acts a bit like a singleton
+
 class UserProfileInline(admin.StackedInline):
+    """
+    Define an inline admin descriptor for UserProfile model
+    which acts a bit like a singleton
+    """
     model = UserProfile
     can_delete = False
     verbose_name_plural = 'leaderboard_entry'
 
-# Define a new User admin
+
 class UserAdmin(BaseUserAdmin):
+    """
+    Define a new User admin
+    """
     inlines = (UserProfileInline, )
+
 
 # Re-register UserAdmin
 admin.site.unregister(User)
--- api/validation.py
+++ api/validation.py
@@ -1,7 +1,5 @@
-from django.core.exceptions import ValidationError
-from django.core.validators import *
+from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator, EmailValidator
 
-import re
 
 USERNAME_MIN_LENGTH = 1
 USERNAME_MAX_LENGTH = 16
@@ -12,6 +10,25 @@ PASSWORD_MAX_LENGTH = 24
 EMAIL_MIN_LENGTH = 4
 EMAIL_MAX_LENGTH = 24
 
-USERNAME_VALIDATORS = [MinLengthValidator(USERNAME_MIN_LENGTH), MaxLengthValidator(USERNAME_MAX_LENGTH), RegexValidator(regex=USERNAME_REGEX, message=["Invalid username.", "Username must only consist of alphanumeric characters, dashes or underscores."])]
-PASSWORD_VALIDATORS = [MinLengthValidator(PASSWORD_MIN_LENGTH), MaxLengthValidator(PASSWORD_MAX_LENGTH)]
-EMAIL_VALIDATORS = [MinLengthValidator(EMAIL_MIN_LENGTH), MaxLengthValidator(EMAIL_MAX_LENGTH), EmailValidator()]
+USERNAME_VALIDATORS = [
+    MinLengthValidator(USERNAME_MIN_LENGTH),
+    MaxLengthValidator(USERNAME_MAX_LENGTH),
+    RegexValidator(
+        regex=USERNAME_REGEX,
+        message=[
+            "Invalid username.",
+            "Username must only consist of alphanumeric characters, dashes or underscores."
+        ]
+    )
+]
+
+PASSWORD_VALIDATORS = [
+    MinLengthValidator(PASSWORD_MIN_LENGTH),
+    MaxLengthValidator(PASSWORD_MAX_LENGTH)
+]
+
+EMAIL_VALIDATORS = [
+    MinLengthValidator(EMAIL_MIN_LENGTH),
+    MaxLengthValidator(EMAIL_MAX_LENGTH),
+    EmailValidator()
+]
